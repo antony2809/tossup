0d4b654f3a52c38f1c384e26d3b1ddfd994a5a08
Diaz <aydz@gft.com>
2020-01-24 13:49:53 +0100

    Added basic interfaces

--- libs/api-interfaces/src/lib/draw.entity.ts
+++ libs/api-interfaces/src/lib/draw.entity.ts
@@ -1,1 +1,18 @@
-export class Draw {}
+import { ID } from '@datorama/akita';
+import { User } from './user.entity';
+
+export class Draw {
+  id!: ID;
+  created!: Date;
+  updated!: Date;
+  // Fecha limite del usuario para apuntarse
+  limitDate!: Date;
+  // Fecha en la que se celebrar el sorteo;
+  drawDate!: Date;
+  cost!: number;
+  name!: string;
+  description!: string;
+  participations: User[];
+  winner!: User;
+  status!: 'open' | 'closed' | 'done' | 'canceled';
+}
--- libs/api-interfaces/src/lib/user.entity.ts
+++ libs/api-interfaces/src/lib/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { ID } from '@datorama/akita';
+import { Draw } from './draw.entity';
+
+export class User {
+  id!: ID;
+  created!: Date;
+  updated!: Date;
+  firstname!: string;
+  lastname!: string;
+  totalPoints!: number;
+  draws?: Draw[];
+  preferences!: UserPreferences;
+  email!: string;
+  phone!: string;
+  paymentMethods!: UserPaymentMethod[];
+}
+
+export class UserPreferences {
+  invoiceInformation!: UserInvoiceInformation;
+}
+
+export class UserPaymentMethod {}
+
+export class UserInvoiceInformation {
+  country!: string;
+  address!: string;
+  addressMore!: string;
+  province!: string;
+  postalCode!: string;
+  city!: string;
+}
